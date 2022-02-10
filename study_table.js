1f66f240b1df099615fb2275eb4d06040f062647
VedTheBest2011 <82335213+VedTheBest2011@users.noreply.github.com>
2022-02-10 13:56:22 +0400

    Add files via upload

---
+++ study_table.js
@@ -0,0 +1,34 @@
+status="";
+img="";
+
+function preload()
+{
+    img = loadImage("study_table.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 350);
+    canvas.center()
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Status: Detecting Objects...";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    object_detector.detect(img, gotResults);
+}
+
+function gotResults(results, error)
+{
+    if(error){
+        console.log(error);
+    }
+    console.log(results)
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 350 )
+}
